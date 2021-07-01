529036339b92213c0cd3bce526145c23528cb049
Edwin Luong <edwinluong@Edwins-MacBook-Pro.local>
2021-07-01 15:33:16 -0700

    message

--- pages/index.js
+++ pages/index.js
@@ -43,22 +43,22 @@ export default function Home() {
       <ul2 style={{"list-style-type":"none"}}>
 
       <h3 style={{"text-align":"center"}}>
-      <li>
+      <li style={{"padding":"12px"}}>
         <Image width={250} height={350} src="item3373.png"  alt="Item  #3373"></Image>
         <h5 style={{"color": "#000000","font-family": "helvetica","font-size": "20px"}}><p>#3373</p></h5>
       </li>
 
-      <li>
+      <li style={{"padding":"12px"}}>
         <Image width={300} height={300} src="item2424.png"  alt="Item  #2424"></Image>
         <h5 style={{"color": "#000000","font-family": "helvetica","font-size": "20px"}}><p>#2424</p></h5>
       </li>
 
-      <li>
+      <li style={{"padding":"12px"}}>
         <Image width={300} height={200} src="item8242.png"  alt="Item  #8242"></Image>
         <h5 style={{"color": "#000000","font-family": "helvetica","font-size": "20px"}}><p>#8242</p></h5>
       </li>
 
-      <li>
+      <li style={{"padding":"12px"}}>
         <Image width={250} height={350} src="item0482.png"  alt="Item  #0482"></Image>
         <h5 style={{"color": "#000000","font-family": "helvetica","font-size": "20px"}}><p>#0482</p></h5>
       </li>
